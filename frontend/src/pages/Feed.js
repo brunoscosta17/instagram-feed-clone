fbaf707044e4b19e54c6738f9ba9f369e8e8cc4d
= <=>
2019-06-13 21:06:48 -0300

    Finalized New Post component

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api.js';
 
 import './Feed.css';
 import more from '../assets/more.svg';
@@ -7,53 +8,43 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+
+    state = {
+        feed: [],
+    }
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({ feed: response.data })
+    }
+
     render() {
-        return(
+        return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>user.name</span>
-                            <span className="place">user.place</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="http://localhost:3333/files/fairtek.jpg" alt="" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
+                {this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais" />
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
 
-                        <strong>980 curtidas</strong>
-                        <p>Um post
-                            <span>#react</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>user.name</span>
-                            <span className="place">user.place</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="http://localhost:3333/files/fairtek.jpg" alt="" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-
-                        <strong>980 curtidas</strong>
-                        <p>Um post
-                            <span>#react</span>
-                        </p>
-                    </footer>
-                </article>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>{post.description}
+                            <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
             </section>
         )
     }
